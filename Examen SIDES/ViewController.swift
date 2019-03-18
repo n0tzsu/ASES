f8c12fc16c11b9bbea2fece1a4a0980f86fb7e82
Raphael Chauvin <dev.raphael.chauvin@univ-rouen.fr>
2019-03-18 11:12:04 +0100

    Initial Commit

---
+++ Examen SIDES/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examen SIDES
+//
+//  Created by Raphaël Chauvin on 18/03/2019.
+//  Copyright © 2019 UFR SANTE ROUEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Examen SIDESTests/Examen_SIDESTests.swift
@@ -0,0 +1,34 @@
+//
+//  Examen_SIDESTests.swift
+//  Examen SIDESTests
+//
+//  Created by Raphaël Chauvin on 18/03/2019.
+//  Copyright © 2019 UFR SANTE ROUEN. All rights reserved.
+//
+
+import XCTest
+@testable import Examen_SIDES
+
+class Examen_SIDESTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
