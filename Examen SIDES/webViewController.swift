1f21c42a2e057f246e35fc592d38354a7978b714
Raphael Chauvin <dev.raphael.chauvin@univ-rouen.fr>
2019-03-29 16:15:55 +0100

    webView from SB finally works, no need to work on UIView anymore

--- Examen SIDES/webViewController.swift
+++ Examen SIDES/webViewController.swift
@@ -3,28 +3,25 @@
 //  Examen SIDES
 //
 //  Created by Raphaël Chauvin on 11/09/2017.
-//  Copyright © 2017 UFR SANTE ROUEN. All rights reserved.
+//  Copyright © 2019 UFR SANTE ROUEN. All rights reserved.
 //
 
 import WebKit
 
 class webViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
     
-    @IBOutlet var webViewContainer: UIView!
     @IBOutlet var webView: WKWebView!
+    
     var mode = 1
-
-    func load() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        webView.uiDelegate = self
-        view = webView
-        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        load()
-        let urlString:String = "https://www.google.fr"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let urlString:String = "https://side-sante.fr/learning/exam/index"
         let url:URL = URL(string: urlString)!
         let urlRequest:URLRequest = URLRequest(url: url)
         webView.load(urlRequest)
@@ -40,14 +37,12 @@ class webViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
             let value = UIInterfaceOrientation.landscapeLeft.rawValue
             UIDevice.current.setValue(value, forKey: "orientation")
             mode = 2
-            //webView.reload()
         }
         else if mode == 2 {
             appDelegate.deviceOrientation = .portrait
             let value = UIInterfaceOrientation.portrait.rawValue
             UIDevice.current.setValue(value, forKey: "orientation")
             mode = 1
-            //webView.reload()
         }
     }
     @IBAction func goBack(_ sender: Any) {
